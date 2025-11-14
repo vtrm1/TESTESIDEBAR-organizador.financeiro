34cd2528b865fe667b238a301afd6579a401c7e9
vtrm1 <88904096+vtrm1@users.noreply.github.com>
2025-11-13 22:27:14 -0300

    Add default Firebase web config for local usage

---
+++ app-config.js
@@ -0,0 +1,23 @@
+window.__FIREBASE_CONFIG__ = {
+    apiKey: "AIzaSyCpBfq801WlkxgHhWwORDvnKWpXGXz3k4c",
+    authDomain: "organizacao-financeira1.firebaseapp.com",
+    projectId: "organizacao-financeira1",
+    storageBucket: "organizacao-financeira1.firebasestorage.app",
+    messagingSenderId: "896359450111",
+    appId: "1:896359450111:web:bd8d7c2f2e35d4f27a93ca",
+    measurementId: "G-58ZVECNK3B"
+};
+
+window.__APP_CONFIG__ = {
+  accessControl: {
+    adminUids: [],
+    adminEmails: [],
+    allowedEmails: [],
+    allowedDomains: [],
+    requireApproval: true,
+    requireAllowlist: false,
+  },
+  firebase: {
+    client: null,
+  },
+};
